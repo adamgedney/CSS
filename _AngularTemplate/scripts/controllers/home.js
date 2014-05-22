7e35690ffe4fbddaf8bc69f4c64fc4462c2bc9f7
Adam <adam.gedney@gmail.com>
2014-05-22 18:27:24 -0400

    added small lighweight angular template

---
+++ _AngularTemplate/scripts/controllers/home.js
@@ -0,0 +1,21 @@
+
+//==========================================//
+//Home Controller
+//==========================================//
+
+App.controller('home', ['$scope', function($scope){
+
+
+	$scope.test = {
+		color 	: 'red',
+		size 	: 'large',
+		weight 	: '12oz'
+	};
+
+	$scope.title = 'my eggs'
+
+
+
+
+
+}]);
---
+++ _AngularTemplate/views/home.tpl
@@ -0,0 +1,15 @@
+
+		<header>
+			<h1>{{title}}</h1>
+		</header>
+
+			<section>
+				<ul>
+					<li ng-repeat="t in test">{{t}}</li>
+				</ul>
+			</section>
+
+		<footer>
+		</footer>
+
+
